11d5c7ff679c2bc43d7a903fc6c038aff0f80229
Christian Andersen <c.andersen2012@gmail.com>
2016-12-24 16:27:48 -0600

    conf

---
+++ config.py
@@ -0,0 +1,36 @@
+'''
+Created on Dec 24, 2016
+
+@author: christian
+'''
+import logging
+import sys
+from logging import handlers
+
+# Level for the logger to log to the console.
+loggingLevel=logging.INFO
+
+# name of the logging file
+loggingFile='gplayservice.log'
+
+# gplay login credentials
+gplayUsername='c.andersen2012@gmail.com'
+gplayPassword='Abs14qp99z!'
+
+# Flask options
+port = 5000
+host='0.0.0.0'
+
+
+# logging logging configuration
+log = logging.getLogger('__name__')
+log.setLevel(loggingLevel)
+format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setFormatter(format)
+log.addHandler(ch)
+
+fh = handlers.RotatingFileHandler(loggingFile, maxBytes=(1048576*5), backupCount=7)
+fh.setFormatter(format)
+log.addHandler(fh)
